db62c87ceb574965875589919cae8cc166f8deb4
Stephen O'Kennedy <stephenoken@gmail.com>
2015-10-02 17:26:00 +0100

    "Added Client messaging feature"
    ""

--- client/index.js
+++ client/index.js
@@ -1,11 +1,34 @@
-var socket = require('socket.io-client');
+var io = require('socket.io-client');
+var readline = require('readline');
+var moment = require('moment');
 
-socket.connect("http://localhost:3000",function () {
-  
+var socket = io.connect("http://localhost:3000");
+socket.on('welcome',function (data) {
+  console.log(data);
+});
+function sendMessage(answer) {
+  socket.emit('chat message',{
+    message: answer,
+    time : moment().format('MMMM Do YYYY, h:mm:ss a')
+  });
+}
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
 
-// socket.on('connect', function(){
-// });
+var recursiveMessage = function () {
+  rl.question("Send Message ", function(answer) {
+    if(answer ==="quit")
+      rl.close();
+    sendMessage(answer);
+    console.log("Sent", answer);
+    recursiveMessage();
+  });
+}
+
+recursiveMessage();
 // socket.on('event', function(data){
 // });
 // socket.on('disconnect', function(){});
--- index.js
+++ index.js
@@ -7,9 +7,21 @@ app.get('/',function (req,res) {
   res.send('<h1>Hello World</h1>');
 })
 io.on('connection',function (socket) {
+  socket.emit('welcome',"Hello Client " + socket.id)
   console.log('a user connected');
+
+  socket.on('chat message', function(response){
+    console.log(response.time+' message: ' + response.message);
+  });
+
+  socket.on('disconnect',function () {
+    console.log("a user disconnected");
+  })
 });
 
+io.on('disconnect',function () {
+  console.log('user disconnected');
+});
 http.listen(3000,function () {
   var host = http.address().address;
   var port = http.address().port;
